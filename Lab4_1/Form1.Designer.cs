5a9374cfb82e9568a7b7441276b1ed5495a81505
Elizaveta1412 <79195511+Elizaveta1412@users.noreply.github.com>
2021-04-02 21:05:57 +0300

    Add files via upload

---
+++ Lab4_1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace GDIPainter
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.StopBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StopBTN
+            // 
+            this.StopBTN.Location = new System.Drawing.Point(91, 15);
+            this.StopBTN.Name = "StopBTN";
+            this.StopBTN.Size = new System.Drawing.Size(65, 34);
+            this.StopBTN.TabIndex = 1;
+            this.StopBTN.Text = "Stop";
+            this.StopBTN.UseVisualStyleBackColor = true;
+            this.StopBTN.Click += new System.EventHandler(this.StopBTN_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(233, 62);
+            this.Controls.Add(this.StopBTN);
+            this.Controls.Add(this.button1);
+            this.Name = "FormMain";
+            this.Text = "GDIPainter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button StopBTN;
+    }
+}
+
---
+++ Lab4_1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDIPainter
+{
+    public partial class FormMain : Form
+    {
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+        
+        IntPtr d = GetDC(IntPtr.Zero);
+
+        Graphics gfx = Graphics.FromHdc(GetDC(IntPtr.Zero));
+
+        bool WorkStop = true;
+
+        Thread thread;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WorkStop = true;
+            thread = new Thread(new ThreadStart(drawOnDesktop));
+            thread.Start();
+        }
+
+        private void drawOnDesktop()
+        {
+            Point lastPoint = new Point(500, 500);
+
+            while (WorkStop)
+            {
+                Point p = new Point(Cursor.Position.X, Cursor.Position.Y);
+
+                gfx.DrawLine(new Pen(Brushes.DeepSkyBlue), lastPoint, p);
+                lastPoint = p;
+            }
+        }
+
+        private void Back()
+        {
+            while (true)
+            {
+                Thread.Sleep(2000);
+            }
+        }
+
+        private void StopBTN_Click(object sender, EventArgs e)
+        {
+            WorkStop = false;
+            thread = new Thread(new ThreadStart(Back));
+        }
+    }
+}
